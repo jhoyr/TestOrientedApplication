8eb84c66220490bca6cabb3598c7179301ae45b1
Jhoy Rodrigues <jhoymartins@gmail.com>
2024-08-06 17:59:37 -0300

    Add project files.

---
+++ TestOrientedApplication/Functions/Calculator.cs
@@ -0,0 +1,12 @@
+﻿namespace TestOrientedApplication.Functions;
+
+public class Calculator
+{
+    public void Sum()
+    {
+
+        int result = calculator.Sum(3,5);
+
+        Assert.AreEqual(8, result);
+    }
+}
---
+++ TestOrientedApplication/TestFunctions/TestCalculator.cs
@@ -0,0 +1,24 @@
+﻿namespace TestOrientedApplication.TestFunctions;
+
+public class TestCalculator
+{
+
+    [TestClass]
+    public class Calculator
+    {
+        /*
+         *  
+         *
+         */
+        [TestMethod]
+        public void SumTest()
+        {
+            Calculator calculator = new Calculator();
+
+            int result = calculator.Sum(3, 5);
+
+            Assert.AreEqual(8, result);
+        }
+    }
+
+}
